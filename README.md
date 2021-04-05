f442f22b31cbe88e01f1c7a1f9898035cc43f1d5
Richard <richardyu042@gmail.com>
2021-04-04 23:53:51 -0400

    automated file running for testing

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 dont use test.sh, it is just a reference for the final test setup
 
 stack test should work now ish
+run stack test --test-arguments "--no-create"
+if there is only .rkt files in test folder as this will prevent golden tests from auto generating, since we have a canonical compiler (racket-lang)
 
 canonical.sh should run racket on each rkt file in features and generate ref (erence) files. 
 todo - 	integrate canonical generation in stack test framework. 
--- test/Regression.hs
+++ test/Regression.hs
@@ -1,3 +1,4 @@
+{-# Language OverloadedStrings #-}
 module Main where
 
 import Test.Tasty
@@ -8,6 +9,8 @@ import System.FilePath
 import System.Process
 import System.Directory
 
+import qualified Turtle
+
 import Parser
 import Compile
 
@@ -15,15 +18,24 @@ main :: IO()
 main = do
 	testSetup
 	defaultMain =<< diffTest
+	--remove .run and .res files after running (maybe remove .ref? would allow for changing tests)
+	--all are removed since they change with each compiler change
+	--TODO only remove if test passes
+	cleanDir "./test/regression/features" [".run", ".res"]
+
+--removes reference files
+fullClean :: IO()
+fullClean = cleanDir "./test/regression/features" [".run", ".res", ".ref"]
 
 diffTest :: IO TestTree
 diffTest = do
 	-- gets all result files (compiled and run files)
-	regressions <- findByExtension [".ref"] "./test/regression/features"
-	-- list comprehension running golden diff for each result file generated. 
+	regressions <- findByExtension [".rkt"] "./test/regression/features"
+	-- list comprehension running golden diff for each reference file found
 	return $ testGroup "diff golden tests" [ goldenVsFile ("diffing " ++ (takeBaseName golden)) golden output dummy 
-		| golden <- regressions, let output = replaceExtension golden ".res"]
+		| golden <- replaceExt regressions ".ref", let output = replaceExtension golden ".res"]
 
+-- does nothing since makefile calls the compilation
 dummy :: IO ()
 dummy = return ()
 
@@ -31,13 +43,36 @@ testSetup :: IO ()
 testSetup = do
 	-- get all racket files from top level
 	regressions <- findByExtension [".rkt"] "./test/regression/features"
-	--make filepath absolute
+	-- make filepath absolute
 	regressions <- mapM makeAbsolute regressions
+	-- generate canonical (reference) output files
+	compileReferences regressions
 	-- compile all racket files after changing the extensions to .run for make
-	compileFiles $ replaceExt regressions "run"
+	compileFiles $ replaceExt regressions ".run"
+	-- generate result files for diffing
+	-- TODO probably need turtle for this
+	-- runExes (replaceExt regressions ".run") (replaceExt regressions ".res")
+	_ <- Turtle.shell "./test/canonical.sh" Turtle.empty
+	print "test setup complete"
+
+-- should never be differing list size since both are transformed from same list
+runExes :: [FilePath] -> [FilePath] -> IO ()
+runExes [] [] = print "files run"
+runExes [x] [y] = runExe x y
+runExes (x:xs) (y:ys) = do
+	runExe x y
+	runExes xs ys
+
+
+-- no existence check since it may have to be updated
+runExe :: FilePath -> FilePath -> IO ()
+runExe run res = do
+	let comm = run ++ " > " ++ res
+	_ <- createProcess (shell comm)
+	print ("generate result file from " ++ run)
+
 	
-	
---calls make on each filepath given. 
+--calls make on each filepath given. Generates executable compiled .run files
 compileFiles :: [FilePath] -> IO ()
 compileFiles [] = print "files compiled"
 compileFiles [x] = do
@@ -51,6 +86,9 @@ compileFiles (x:xs) = do
 replaceExt :: [FilePath] -> String -> [FilePath]
 replaceExt (x:xs) str = (replaceExtension x str) : replaceExt xs str
 replaceExt [] _ = []
+
+
+
 
 -- generate canonical (reference) files using racket
 compileReferences :: [FilePath] -> IO ()
@@ -72,7 +110,20 @@ compileReference fp = do
 		
 helper :: FilePath -> FilePath -> IO ()
 helper fp ref = do 
-	_ <- createProcess (proc "racket" [fp, ">", ref]) { cwd = Just "." } -- dont need cwd path since paths are absolute anyway
+	let path = Turtle.decodeString fp
+	let res = Turtle.decodeString ref
+	let cmd = Turtle.format ("racket "Turtle.% Turtle.fp Turtle.%" > "Turtle.% Turtle.fp) path res
+	_ <- Turtle.shell cmd Turtle.empty
+--	_ <- createProcess (proc "racket" [fp, ">", ref]) { cwd = Just "." } -- dont need cwd path since paths are absolute anyway
 	print ("compiled canonical output for " ++ fp)
 
+
+
+-- remove all files with extensions in filepath directory
+cleanDir :: FilePath -> [FilePath] -> IO ()
+cleanDir fp str = do
+	res <- findByExtension str fp
+	mapM removeFile res
+	--print output here?
+	return ()
 
